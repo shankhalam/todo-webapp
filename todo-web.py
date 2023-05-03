a7bba0904971c16ab8c332cabe0d4dce783cdb83
shankhalam <shankhalamh@gmail.com>
2023-05-03 22:57:24 +0530

    Implement add feature.

--- todo-web.py
+++ todo-web.py
@@ -3,6 +3,10 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
 
 st.title("ToDo - Web")
 st.subheader("Organise your tasks with Todo Web or Todo Desktop App")
@@ -12,4 +16,5 @@ for todo in todos:
     st.checkbox(todo)
 
 
-st.text_input(label="Enter Your Task", placeholder="Add tasks")
+st.text_input(label="Enter Your Task", placeholder="Add tasks", key="new_todo",
+               on_change=add_todo)
