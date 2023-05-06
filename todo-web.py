1c0fb4de74516a95e351894f5eb0f3ce60bf819e
shankhalam <shankhalamh@gmail.com>
2023-05-06 21:05:34 +0530

    add issue fix

--- todo-web.py
+++ todo-web.py
@@ -16,16 +16,19 @@ st.subheader("Organise your tasks with Todo Web or Todo Desktop App")
 st.text_input(label="Enter Your Task", placeholder="Add tasks", key="new_todo",
                on_change=add_todo)
 
-# st.divider()
+if st.text_input:
+    for index, todo in enumerate(todos):
+        checkbox = st.checkbox(todo, key=todo)
+        if checkbox:
+            todos.pop(index)
+            functions.write_todos(todos)
+            to_do = st.session_state[todo]
+            del to_do
+            st.experimental_rerun()
+else:
+    st.info("No text to Add")
 
-for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
-    if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos)
-        to_do = st.session_state[todo]
-        del to_do
-        st.experimental_rerun()
+
 
 
 
